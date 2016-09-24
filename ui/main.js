3d06e022eb03b352569818233f653aefbc767519
riya13 <riya_jindal@outlook.com>
2016-09-24 17:54:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,13 @@
 console.log('Loaded!');
 
+//Changing text of the main-text div
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+//Moving image of id madi
+var img = document.getElementById('madi');
+
+img.onclick  = function() {
+    img.style.marginLeft = '100px';
+};
