1c28a980b3cbc611ddb63e9e728a2e4380ea846a
riya13 <riya_jindal@outlook.com>
2016-09-24 18:48:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter++;
+    res.send(counter.toString());
+});
+
+
 app.get('/:articleName', function (req, res) {
     
     var articleName = req.params.articleName;
