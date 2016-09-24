607b521b8125380e6112ba8000618c8949499e35
riya13 <riya_jindal@outlook.com>
2016-09-24 17:17:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,6 +126,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'mainjs'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
