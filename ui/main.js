69b94c8c12a441eb06e3dbaa8f35fb20cd5f2856
riya13 <riya_jindal@outlook.com>
2016-09-24 18:02:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,14 @@ element.innerHTML = 'New Value';
 //Moving image of id madi
 var img = document.getElementById('madi');
 
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+
 img.onclick  = function() {
-    img.style.marginLeft = '100px';
+    var inetrval = setInterval(moveRight, 100);
 };
