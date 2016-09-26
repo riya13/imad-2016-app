65c4d48f4298dd16a182c909b3a9f6e885c85afd
riya13 <riya_jindal@outlook.com>
2016-09-26 13:52:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,108 +7,77 @@ app.use(morgan('combined'));
 
 
 var articles = {
-    
-
-   'article-one' : {
-    title: 'Article-One | Riya Jindal',
-    heading: 'Article-One',
-    date: '21 Sept 2016',
-    content:  
-       `<p>
-            Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information.
-        </p>
-        <p>
-            Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information.
-        </p>
-        <p>
-            Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information. Hey there! Welcome to my first article. This is where you get all the concerned information.
-        </p>`,
-   
-  },
-  
-  
-   'article-two' : { 
-    title: 'Article-Two | Riya Jindal',
-    heading: 'Article-Two',
-    date: '23 Sept 2016',
-    content:  
-       `<p>
-            Hey there! Welcome to my second article. This is where you get all the concerned information. 
-        </p>`,
-   },
-   
-   
-   'article-three' : {
-    title: 'Article-Three | Riya Jindal',
-    heading: 'Article-Three',
-    date: '28 Sept 2016',
-    content:  
-       `<p>
-            Hey there! Welcome to my third article. This is where you get all the concerned information. 
-        </p>`,
-   
- },
-
-
+    'article-one': {
+      title: 'Article One | Riya Jindal',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: `
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>`
+    },
+    'article-two': {
+      title: 'Article Two | Riya Jindal',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: `
+          <p>
+              This is the content for my second article.
+          </p>`
+    },
+    'article-three': {
+      title: 'Article Three | Riya Jindal',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: `
+          <p>
+              This is the content for my third article.
+          </p>`
+    }
 };
 
-
 function createTemplate (data) {
-    
     var title = data.title;
-    var heading = data.heading;
     var date = data.date;
+    var heading = data.heading;
     var content = data.content;
     
-    
-    var htmlTemplate = 
-    `<html>
-
-    <head>
-    <title>
-       ${title}
-    </title>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    
-    <link href="/ui/style.css" rel="stylesheet" />
-    
-    
-    </head>
-
-
-    <body>
-
-    <div class="container">
-    <div>
-        <a href="/">Home</a>
-    </div>
-    
-    <hr />
-    
-    <div>
-        <h3>${heading}</h3>
-    </div>
-    
-    <div>
-         ${date}
-    </div>
-    
-    <div>
-        
-         ${content}
-        
-    </div>
-    </div>
-</body>
-
-</html>`
-;
-
-return htmlTemplate;
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
 }
-
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -116,28 +85,26 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function (req, res) {
-    counter++;
-    res.send(counter.toString());
+   counter = counter + 1;
+   res.send(counter.toString());
 });
 
 var names = [];
-app.get('/submit-name', function (req, res) {
-    //Get names from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
+  // Get the name from the request
+  var name = req.query.name;
+  
+  names.push(name);
+  // JSON: Javascript Object Notation
+  res.send(JSON.stringify(names));
 });
-
 
 app.get('/:articleName', function (req, res) {
-    
-    var articleName = req.params.articleName;
-    
+  // articleName == article-one
+  // articles[articleName] == {} content object for article one
+  var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -150,7 +117,6 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
