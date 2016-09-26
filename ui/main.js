ff0ec24920bde4af645b3d58606646052dd8583c
riya13 <riya_jindal@outlook.com>
2016-09-26 13:06:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,24 +34,40 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
   
-  //Make a request
+    //Make a request
   
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            
+            //Capture a list of names and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
   
-  //Capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  
-  for(var i=0; i<names.length; i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+            for(var i=0; i<names.length; i++) {
+                list += '<li>' + names[i] + '</li>';
+            } 
+            
+             var ul = document.getElementById('namelist');
+             ul.innerHTML = list;
  
+            
+        }
+    
+    //Making Request
+    request.open('GET', 'http://riya13.imad.hasura-app.io/submit-name?name=' + name , true);
+    request.send(null);
+    
+    
+    };
+
+
 };
-
-
-
 
 
 
