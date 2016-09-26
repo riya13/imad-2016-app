2740fb5ac83afca7772a875d74c1a7ba1f990082
riya13 <riya_jindal@outlook.com>
2016-09-26 13:27:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,75 +1,59 @@
-//counter code
+// Counter code
 var button = document.getElementById('counter');
 
+button.onclick = function () {
 
-button.onclick = function() {
-    
-    //Create a request object
+    // Create a request object
     var request = new XMLHttpRequest();
     
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        
-        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-            
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-      
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();          
+          }
+      }  
+      // Not done yet
     };
-  
-    //Making Request
-    request.open('GET', 'http://riya13.imad.hasura-app.io/counter', true);
+    
+    // Make the request
+    request.open('GET', 'http://coco98.imad.hasura-app.io/counter', true);
     request.send(null);
- 
 };
 
-//Submit name
-
-
+// Submit name
 var submit = document.getElementById('submit_btn');
-
-submit.onclick = function() {
+submit.onclick = function () {
   
-    //Make a request
-  
-    //Create a request object
+    // Create a request object
     var request = new XMLHttpRequest();
     
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        
-        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-            
-            //Capture a list of names and render it as a list
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-  
-            for(var i=0; i<names.length; i++) {
-                list += '<li>' + names[i] + '</li>';
-            } 
-            
-             var ul = document.getElementById('namelist');
-             ul.innerHTML = list;
- 
-            
-        }
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              // Capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;   
+          }
+      }  
+      // Not done yet
+    };
     
-    //Making Request
+    // Make the request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-
-    request.open('GET', 'http://riya13.imad.hasura-app.io/submit-name?name=' + name , true);
-    request.send(null);
-    
-    
-    };
-
+    request.open('GET', 'http://coco98.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);  
 
 };
-
-
-
-
