7b5ee496cea12638416f27cfba06879b0784c021
riya13 <riya_jindal@outlook.com>
2016-09-26 11:14:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,26 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick = function() {
-   counter++;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
-}
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+      
+    };
+  
+    //Making Request
+    request.open('GET', 'http://riya13.imad.hasura-app.io/counter', true);
+    request.send(null);
+ 
+};
