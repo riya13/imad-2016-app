ec81042fe54a607f32f61b49d4716e137fc0ab08
riya13 <riya_jindal@outlook.com>
2016-10-30 17:30:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'riya13',
+  database: 'riya13',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -81,6 +91,23 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req, res) {
+   //Make a SELECT query
+   //return the response
+   
+   pool.query('SELECT * FROM test', function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else {
+           res.send(JSON.stringify(result));
+       }
+  });
+   
 });
 
 var counter = 0;
