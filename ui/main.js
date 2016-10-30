88664b08fbe5751624dcddd50fedcd9e39f12efb
riya13 <riya_jindal@outlook.com>
2016-10-30 17:47:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,46 +59,6 @@ submit.onclick = function () {
 };
 
 
-//Submit and Display Comments
-var output = document.getElementById('submit-btn');
-output.onClick = function() {
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        
-        if(request.readyState === XMLHttpRequest.DONE) {
-            
-            if(request.status === 200) {
-                
-                //Capture the comments
-                
-                var comments = request.responseText;
-                comments = JSON.parse(comments);
-                
-                var list = '';
-                for(var i=0; i<comments.length; i++) {
-                    list += '<li>' + comments[i] + '</li>';
-                }
-                
-                var ul = document.getElementById('commentlist');
-                ul.innerHTML = list;
-                
-            }
-        }
-    };
-    
-    //Making Request
-    
-    var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-    request.open('GET', 'http://riya13.imad.hasura-app.io/submit-comment?comment=' + comment, true);
-    request.send(null); 
-    
-};
-
 
 
 
