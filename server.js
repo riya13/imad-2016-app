a4f34edba6f417c446a16654b5032f8b3087ddc8
riya13 <riya_jindal@outlook.com>
2016-10-30 20:29:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/articles/:articleName', function (req, res) {
               res.status(404).send('Article not found.');
           }
           else{
-              var articleData = result.rows;
+              var articleData = result.rows[0];
               res.send(createTemplate(articleData));
           }
       }
