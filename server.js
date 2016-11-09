4aae89c3cb4f3a27b2686c2fc4090469fcd0cf49
riya13 <riya_jindal@outlook.com>
2016-11-09 16:13:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,6 +144,11 @@ app.get('/check-login', function(req, res){
    }
 });
 
+app.get('/logout', function(req, res) {
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
 
 var pool = new Pool(config);
 
