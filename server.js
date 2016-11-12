44ddc27a9b9fe91ffab2a20760bdd289e2280142
riya13 <riya_jindal@outlook.com>
2016-11-12 22:08:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,15 +137,16 @@ app.post('/login', function(req, res) {
 });
 
 app.get('/check-login', function(req, res){
-   if(req.session && req.session.auth && req.session.auth.userId) {
-      //Load the user object
-     pool.query('SELECT * FROM "user" WHERE id = $1' , [req.session.auth.userId] , function(req, res) {
-       if(err) {
-         res.status(500).send(err.toString());
-       }
-       else {
-         res.send(result.rows[0].username);
-   }); 
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } 
        else {
        res.send('You are not logged in');
    }
