624d71d5d741ed6e3a89e65f1e432c40d637e789
riya13 <riya_jindal@outlook.com>
2016-11-26 11:05:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,16 +4,7 @@ function loadLoginForm () {
         
         
         
-        <!DOCTYPE html>
-<html lang="en">
-<head>
   
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  
-</head>
-<body>
 
 <div class="container">
   <h2>Horizontal form</h2>
@@ -50,8 +41,7 @@ function loadLoginForm () {
   </form>
 </div>
 
-</body>
-</html> `;
+ `;
 
         
         
